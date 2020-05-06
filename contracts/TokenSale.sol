ffc9a71ac3d1f1bee9222309994579553b779959
pythonpete32 <pythonpete32@gmail.com>
2020-05-06 03:20:36 +0100

    Add hardcap

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -4,8 +4,13 @@ import "@aragon/apps-token-manager/contracts/TokenManager.sol";
 import "@aragon/apps-agent/contracts/Agent.sol";
 
 
+
 contract TokenSale is AragonApp {
     using SafeMath for uint256;
+
+    // Errors
+    string private constant ERROR_EXCEEDED_HARDCAP = "ERROR_EXCEEDED_HARDCAP";
+
 
     // Roles
     bytes32 constant public SET_TOKEN_MANAGER_ROLE = keccak256("SET_TOKEN_MANAGER_ROLE");
@@ -15,6 +20,9 @@ contract TokenSale is AragonApp {
     TokenManager public tokenManager;
     Agent public agent;
     uint256 public rate; // token units per wei
+    uint256 public cap;
+    uint256 public tokensSold;
+
 
     // Events
     event TokensPurchased(address buyer, uint256 value, uint256 amount);
@@ -24,10 +32,12 @@ contract TokenSale is AragonApp {
     /**
      * @param _tokenManager TokenManager for minted token
      */
-    function initialize(TokenManager _tokenManager, Agent _agent, uint256 _rate) public onlyInit {
+    function initialize(TokenManager _tokenManager, Agent _agent, uint256 _rate, uint256 _cap) public onlyInit {
         tokenManager = _tokenManager;
         agent = _agent;
         rate = _rate;
+        cap = _cap;
+        tokensSold = 0;
 
 
         initialized();
@@ -41,7 +51,10 @@ contract TokenSale is AragonApp {
      * @notice mint equal tokens for eth
      */
     function mint(address to, uint256 value) public payable {
+        require(tokensSold.add(value) < cap, ERROR_EXCEEDED_HARDCAP);
+
         uint256 amount = rate.mul(value);
+        tokensSold = tokensSold.add(amount);
         tokenManager.mint(to, amount);
         agent.deposit.value(value)(ETH, value);
         emit TokensPurchased(to, value, amount);
---
+++ scripts/getABI.js
@@ -0,0 +1,3 @@
+const artifact = require('../artifacts/TokenSale.json')
+
+console.log(JSON.stringify(artifact.abi))
