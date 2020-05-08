1dd7dbb1788be13be87742ef155c0f81a0cc4f27
pythonpete32 <pythonpete32@gmail.com>
2020-05-08 14:54:39 +0100

    add contractState script

---
+++ scripts/contractState.js
@@ -0,0 +1,40 @@
+let ethers = require('ethers')
+const BigNumber = require('bignumber.js');
+
+
+const main = async () => {
+    const url = "http://localhost:8545";
+    const privateKey = '0xa8a54b2d8197bc0b19bb8a084031be71835580a01e70a45a13babd16c9bc1563'
+    const TOKENSALE_CONTRACT = '0x8f1cf28dcd72f5cf75091e61bf99f7ac2edb9dac'
+    const provider = new ethers.providers.JsonRpcProvider(url);
+    let wallet = new ethers.Wallet(privateKey, provider)
+
+    wallet = wallet.connect(provider)
+
+
+    let abi = [
+        'function isOpen() public view returns (bool)',
+        'function rate() public view returns (uint256)',
+        'function tokensSold() public view returns (uint256)',
+        'function cap() public view returns (uint256)'
+
+    ]
+
+    let sale = new ethers.Contract(TOKENSALE_CONTRACT, abi, wallet)
+    let open = await sale.isOpen()
+    console.log(`sale open:    ${open}`)
+    console.log('tokens sold: ', + BigNumber(await sale.tokensSold()).toString())
+    console.log('tokens cap:  ', + BigNumber(await sale.cap()).toString())
+    console.log('tokens rate: ', + BigNumber(await sale.rate()).toString())
+
+//    console.log(await sale.cap().integerValue())
+ //   console.log(await sale.rate().integerValue())
+    //console.log(await sale.isOpen())
+
+    
+
+    //console.log(tx)
+    //console.log('Balance #1: ', ethers.utils.formatEther(amount1))
+}
+
+main()
